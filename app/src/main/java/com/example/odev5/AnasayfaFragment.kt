55581e5668f84a4a6a27b8d16b052b161131ef77
İdil Öncü <oncuidil@gmail.com>
2024-02-29 18:22:36 +0300

    Toplama işlemi gerçekleşitirildi.Tasarım iyileştirildi.

--- app/src/main/java/com/example/odev5/AnasayfaFragment.kt
+++ app/src/main/java/com/example/odev5/AnasayfaFragment.kt
@@ -9,31 +9,29 @@ import com.example.odev5.databinding.FragmentAnasayfaBinding
 
 class AnasayfaFragment : Fragment() {
     private lateinit var binding: FragmentAnasayfaBinding
+    var toplam=0
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding= FragmentAnasayfaBinding.inflate(inflater, container, false)
 
+        binding.buttonEsittir.setOnClickListener{
+            val sayi= binding.sayi.text.toString()
+            toplam=0
+            val str = sayi.split("+")
 
+            for (parca in str){
+                toplam+=parca.toIntOrNull()?:0
+            }
+            binding.textViewSonuc.text = toplam.toString()
+        }
 
+        binding.buttonSifirla.setOnClickListener{
+            toplam=0
+            binding.textViewSonuc.text="0"
+            binding.sayi.setText("0")
 
-
-
-
-
-
-
-
-
-
+        }
 
 
         return binding.root
-
-
-
-
-
-
-
     }
-
     }
